8a89ee43f5eb27ab67846285353892b690ca0f10
deon-gracias <deongracias1@gmail.com>
2022-07-26 12:16:20 +0530

    Added database scripts

---
+++ backend/lib/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/e-commerce";
+
+// Connect to database
+function connectToDB() {
+    mongoose
+        .connect(mongoURI, {
+            useNewUrlParser: true,
+        })
+        .then(() => console.log("Connected to MongoDB"))
+        .catch((err) => console.log(err));
+}
+
+// Disconnect from database
+function disconnectFromDB() {
+    mongoose.disconnect();
+}
+
+module.exports = { connectToDB, disconnectFromDB };
