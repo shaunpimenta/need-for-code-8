ade46c6426650797b8416dadd0ecd02ff0d19171
deon-gracias <deongracias1@gmail.com>
2022-07-26 14:36:18 +0530

    Added user routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,135 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require("../models/userModel")
+const { isAuth } = require("../lib/auth")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+
+// Get all users (debugging purposes)
+router.get("/", async (req, res, next) => {
+  const users = await User.find({});
+
+  return res.status(200).json(users);
+});
+
+// Profile
+router.get("/profile", isAuth, (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) return next(createError(404, "User id not found"));
+
+  delete user.password;
+  return res.status(201).send(user);
+});
+
+// Delete user
+router.delete("/delete", isAuth, (req, res, next) => {
+  const user = await User.findOneAndDelete({ email: req.user.email });
+
+  if (!user) return next(createError(404, "User not found"));
+
+  delete user.password;
+  return res.status(200).send(user);
+});
+
+// Sign up
+router.post("/signup", (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) return next(createError(409, "User already exists"));
+
+  const createdUser = await new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  }).save();
+
+  // Generate Tokens
+  const tokenData = {
+    name: createdUser.name,
+    email: createdUser.email,
+  },
+    accessToken = generateAccessToken(tokenData),
+    refreshToken = generateRefreshToken(tokenData);
+
+  // Save refresh token in user document
+  createdUser.refreshToken = refreshToken;
+  await createdUser.save();
+
+  delete createdUser.password; // Delete password field
+
+  return res.status(201).send({
+    ...createdUser.toObject({ versionKey: false }),
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+  });
+});
+
+// Sign in
+router.post("/signin", (req, res, next) => {
+  // Check if password is passed in request
+  if (!req.body.password)
+    return next(createError(400, "User Credentials not provided"));
+
+  const user = await User.findOne({ email: req.body.email });
+
+  // User not found
+  if (!user) return next(createError(404, "User not found"));
+
+  // Compare passwords
+  if (!(await user.comparePassword(req.body.password)))
+    return next(createError(401, "Invalid user credentials"));
+
+  // Generate Tokens
+  const tokenData = {
+    name: user.name,
+    email: user.email,
+  },
+    accessToken = generateAccessToken(tokenData),
+    refreshToken = generateRefreshToken(tokenData);
+
+  // Save refresh token in user document
+  user.refreshToken = refreshToken;
+  await user.save();
+
+  // Delete password field
+  delete user.password;
+
+  return res.status(200).send({
+    ...user.toObject({ versionKey: false }),
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+  });
+}
+);
+
+// Sign out
+router.get("/signout", isAuth, (req, res, next) => {
+  const user = await User.findOneAndUpdate(
+    { email: req.user.email },
+    { $unset: { refreshToken: "" } }
+  );
+
+  if (!user) return next(createError(404, "User Not Found"));
+
+  return res.status(200).send({ message: "Successfully signed out" });
+});
+
+// Get new access token
+router.post("/token", (req, res, next) => {
+  const refreshToken = req.body.refreshToken;
+
+  if (!refreshToken || !req.body.email)
+    return next(createError(400, "Insufficient credentials provided"));
+
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return next(createError(404, "User Not Found"));
+
+  if (user.refreshToken !== refreshToken)
+    return next(createError(403, "Invalid Refresh Token"));
+
+  return res.status(200).send({
+    accessToken: generateAccessToken({ name: user.name, email: user.email }),
+  });
 });
 
 module.exports = router;
