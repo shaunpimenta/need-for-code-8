90655122130482bde374529400f9c6c96e997293
deon-gracias <deongracias1@gmail.com>
2022-07-26 21:10:57 +0530

    Added id for product

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -13,19 +13,19 @@ const reviewSchema = new Schema(
 
 const productSchema = new Schema(
     {
+        id: { type: String, required: true },
         name: { type: String, required: true },
-        image: { type: String, required: true },
+        image: { type: String },
         brand: { type: String, required: true },
         price: { type: Number, default: 0, required: true },
         category: { type: String, required: true },
-        tag: { type: String, required: true },
         countInStock: { type: Number, default: 0, required: true },
-        description: { type: String, required: true },
+        description: { type: String },
         rating: { type: Number, default: 0, required: true },
         numReviews: { type: Number, default: 0, required: true },
         reviews: [reviewSchema],
     },
-    { timestamps: true }
+    { timestamps: true, _id: false }
 )
 
 const Product = model('Product', productSchema);
