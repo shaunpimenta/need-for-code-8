bee88e17423e29b1f042cc1e617a4ae66ea5630e
deon-gracias <deongracias1@gmail.com>
2022-07-26 12:18:44 +0530

    Added product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose')
+
+const productSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        image: { type: String, required: true },
+        brand: { type: String, required: true },
+        price: { type: Number, default: 0, required: true },
+        category: { type: String, required: true },
+        tag: { type: String, required: true },
+        countInStock: { type: Number, default: 0, required: true },
+        description: { type: String, required: true },
+        rating: { type: Number, default: 0, required: true },
+        numReviews: { type: Number, default: 0, required: true },
+        reviews: [reviewSchema],
+    },
+    { timestamps: true }
+)
+
+const reviewSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        rating: { type: Number, default: 0 },
+        comment: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Product = model('Product', productSchema);
+
+module.exports = Product;
