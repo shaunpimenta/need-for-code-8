7fe6649d8a3bc5ac6abd23952f8f4f83dd8e759c
deon-gracias <deongracias1@gmail.com>
2022-07-26 12:45:14 +0530

    Removed type in props

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 
-export default function App(props: AppProps) {
+export default function App(props) {
   const { Component, pageProps } = props;
 
   return (
